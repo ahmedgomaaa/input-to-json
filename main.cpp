a3180b4eafaf00a6db46f5ae56c55333617cbbe9
Ahmed Kira <50806393+MrKira128@users.noreply.github.com>
2021-12-04 23:53:48 +0200

    Update main.cpp
    make it better

--- main.cpp
+++ main.cpp
@@ -1,45 +1,19 @@
-#include <iostream>
 #include <fstream>
-using namespace std;
-string aa;
-int ctr=0, cnt=0, uni=0;
-string third() {
-string holder;
-string line;
-
-  ifstream myfile("input.txt");
-  if (myfile.is_open()) {
-    while (getline(myfile, line)) {
-      if (cnt == ctr && cnt==0 ) {
-        aa = aa + "[ \"" + line + "\" "  ;
-        ctr++;}
-         else if (cnt == ctr ) {
-      aa = aa +" , \"" + line + "\" "  ;
-        ctr++;
-
-      }
-
-      else {
-        cnt++;
-        continue;
-      }
-      ctr = ctr + uni;
-      break;
-    }}
-    return aa;}
+#include <string>
+// using namespace std;
 
 int main()
 {
-
-do {
- third();
- uni++; //ctr=  ctr + 2;
-}
-while (ctr != cnt) ;
-aa = aa + "] ";
-cout << aa;
-ofstream output("output.txt");
-output<< aa;
-
-  return 0;
+    std::ifstream in("list.txt");
+    std::ofstream out("list.json");
+    std::string line;
+    out << "[" << "\n";
+    while (getline(in, line))
+    {
+        out << "\"" << line << "\"," << "\n";
+    }
+    out << "]" << "\n";
+    in.close();
+    out.close();
+    return 0;
 }
